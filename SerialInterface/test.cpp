0dfd3b7b539ff508000591375dac2e371cb33e94
leroy_win <1135277606@qq.com>
2020-05-30 19:48:39 +0800

    添加项目文件。

---
+++ SerialInterface/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include "SerialInterface.h"
+#include <windows.h>
+#include <tchar.h>
+
+using namespace std;
+
+int main()
+{
+
+    SerialInterface com;
+
+    if(!com.openSyn("COM1", CBR_9600, NOPARITY,8, ONESTOPBIT))
+    {
+        cout << com.getSerialLastError() << endl;
+        getchar();
+        return 0;
+    }
+
+    if (com.writeStr("Hello World!"))
+    {
+        cout << "send success" << endl;
+    }
+    else
+    {
+        cout << "send fail" << endl;
+    }
+
+    com.closeComm();
+
+
+    //getchar();
+    return 0;    
+}
